ad6c05802265688f6241844a03abda663443f107
Kevin Koech <kevinkoech007@gmail.com>
2025-07-02 13:01:07 +0300

    added script.js,connected to index.html

--- script.js
+++ script.js
@@ -3,3 +3,5 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
+// adding a javascript function in the DOM
+document.querySelector("body").appendChild(h2);
